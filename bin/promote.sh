ab4c0415bbbf8847e84d91d3116028d92cdd3ac4
franklin <2730246+devsecfranklin@users.noreply.github.com>
2022-11-18 22:50:08 -0500

    testing

---
+++ bin/promote.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+declare -a my_files=( "workshop-codemash-2023.pdf" ".vscode/*" )
+
+git checkout workshop
+#for i in "${my_files[@]}"; do git checkout workshop "$i"; done
+git checkout develop tempfile
+#git commit -m ""
