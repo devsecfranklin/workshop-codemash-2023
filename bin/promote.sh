9aaa15d5fa5a764c86c075e72cde0f5bff8e4438
franklin <2730246+devsecfranklin@users.noreply.github.com>
2022-11-18 23:20:43 -0500

    promote script

--- bin/promote.sh
+++ bin/promote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a my_files =[ "workshop-codemash-2023.pdf" ".vscode/*" ]
+declare -a my_files =[ "workshop-codemash-2023.pdf" ".vscode/" "src/" "terraform/" ]
 
 git checkout workshop
 #for i in "${my_files[@]}"; do git checkout workshop "$i"; done
