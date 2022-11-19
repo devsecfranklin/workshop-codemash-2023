b933d010e588926484fba8c54c533ef18b43c330
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2022-11-19 12:42:00 -0500

    Add arch diagrams

--- bin/setup-dev-env.sh
+++ bin/setup-dev-env.sh
@@ -2,6 +2,30 @@
 
 # workshop-codemash-2023 setup script
 
+# v0.1 11/18/2022 first version
+
+# Author:  2730246+devsecfranklin@users.noreply.github.com 
+
+#set -eu
+
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
+
+RED='\033[0;31m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+LBLUE='\033[1;34m'
+CYAN='\033[0;36m'
+LPURP='\033[1;35m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 CONTAINER=false
 
 # Check if we are inside a docker container
