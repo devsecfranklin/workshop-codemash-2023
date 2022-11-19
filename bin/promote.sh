58959018954f5da7583cc99326dc3a9c14cba382
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-11-18 23:28:22 -0500

    testing branch promotions (#25)
    * testing branch promotions
    
    * promote script

--- bin/promote.sh
+++ bin/promote.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-declare -a my_files=( "workshop-codemash-2023.pdf" ".vscode/*" )
+declare -a my_files =[ "workshop-codemash-2023.pdf" ".vscode/" "src/" "terraform/" ]
 
 git checkout workshop
-#for i in "${my_files[@]}"; do git checkout workshop "$i"; done
+for i in "${my_files[@]}"; do git checkout workshop "$i"; done
 git checkout develop tempfile
-#git commit -m ""
+
+git statu
