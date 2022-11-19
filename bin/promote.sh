166298d40b473f4b0c63c1b76e7f153690f981d3
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-11-19 14:49:09 -0500

    Add arch diagrams (#28)
    * Add arch diagrams
    
    * updates to documentation
    
    * updates to documentation
    
    * updates to documentation
    
    * updates to documentation
    
    * testing promote script
    
    * testing promote script
    
    * fix script

--- bin/promote.sh
+++ bin/promote.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-declare -a my_files =[ "workshop-codemash-2023.pdf" ".vscode/" "src/" "terraform/" ]
-
-git checkout workshop
-for i in "${my_files[@]}"; do git checkout workshop "$i"; done
-git checkout develop tempfile
-
-git statu
+declare -a my_files=( "README.md" "lab/workshop-codemash-2023.pdf" ".devcontainer" ".vscode/" "src/" "terraform/" )
+git checkout workshop # We have to start from here
+WK_BRANCH="workshop-$(printf "%(%Y%m%d%H%M%S)T\n" $EPOCHSECONDS)" # make a temp name for workshop branch
+git checkout -b ${WK_BRANCH} # checkout out the new branch name
+for i in "${my_files[@]}"; do git checkout develop "$i"; done
+echo "Now commit and push this branch to workshop"
