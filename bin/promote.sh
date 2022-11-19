e1630aa67a0d140138656b6b0b27825807aebb31
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-11-18 23:48:19 -0500

    remove latex action (#27)
    * testing branch promotions
    
    * take out latex action

--- bin/promote.sh
+++ bin/promote.sh
@@ -5,4 +5,5 @@ declare -a my_files=( "workshop-codemash-2023.pdf" ".vscode/*" )
 git checkout workshop
 #for i in "${my_files[@]}"; do git checkout workshop "$i"; done
 git checkout develop tempfile
+
 #git commit -m ""
