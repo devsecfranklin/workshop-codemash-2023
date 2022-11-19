b862c5a0b6b7c9430b14ada2ff1abedb3878593d
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2022-11-19 14:36:55 -0500

    updates to documentation

--- bin/promote.sh
+++ bin/promote.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-declare -a my_files=( "README.md" "lab/workshop-codemash-2023.pdf" ".vscode/" "src/" "terraform/" )
+declare -a my_files=( "README.md" "lab/workshop-codemash-2023.pdf" ".devcontainer" ".vscode/" "src/" "terraform/" )
 WK_BRANCH="workshop-$(printf "%(%Y%m%d%H%M%S)T\n" $EPOCHSECONDS)"
 git checkout -b ${WK_BRANCH}
-for i in "${my_files[@]}"; do git checkout ${WK_BRANCH} "$i"; done
+for i in "${my_files[@]}"; do git checkout develop "$i"; done
+echo "Now commit and push this branch to workshop"
