1ab742ed599c367bf9bf1be0c92db3b91301f6f1
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2022-11-19 19:31:17 +0000

    testing promote script

--- bin/promote.sh
+++ bin/promote.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-declare -a my_files =[ "workshop-codemash-2023.pdf" ".vscode/" "src/" "terraform/" ]
+declare -a my_files=( "README.md" "lab/workshop-codemash-2023.pdf" ".vscode/" "src/" "terraform/" )
+WK_BRANCH="workshop-$(printf "%(%Y%m%d%H%M%S)T\n" $EPOCHSECONDS)"
+git checkout -b ${WK_BRANCH}
+for i in "${my_files[@]}"; do git checkout ${WK_BRANCH} "$i"; done
 
-git checkout workshop
-for i in "${my_files[@]}"; do git checkout workshop "$i"; done
-git checkout develop tempfile
-
-git statu
